aba2fabfd0e1258eb50a8ded3d1f8e2b24aeae34
Єгор Грушевий <yegorgru@gmail.com>
2020-10-06 11:54:24 +0300

    docs

--- Lab1extra5/docs folder/html/search/all_9.js
+++ Lab1extra5/docs folder/html/search/all_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['less_3c_20book_20_3e_87',['less&lt; Book &gt;',['../structstd_1_1less_3_01_book_01_4.html',1,'std']]],
   ['logduration_88',['LogDuration',['../class_log_duration.html',1,'']]],
-  ['lab1_2bextra5_89',['Lab1+extra5',['../md__readme.html',1,'']]]
+  ['lab1_2bextra5_89',['Lab1+extra5',['../md__c___users_yegor__documents__git_hub__o_o_p__labs_3_semester__lab1extra5__readme.html',1,'']]]
 ];
--- Lab1extra5/docs folder/html/search/pages_0.js
+++ Lab1extra5/docs folder/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['lab1_2bextra5_364',['Lab1+extra5',['../md__readme.html',1,'']]]
+  ['lab1_2bextra5_364',['Lab1+extra5',['../md__c___users_yegor__documents__git_hub__o_o_p__labs_3_semester__lab1extra5__readme.html',1,'']]]
 ];
