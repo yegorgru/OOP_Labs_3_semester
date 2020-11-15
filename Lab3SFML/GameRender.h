3b7ac2e759397792bdf86a059f8329f29cca61c6
Єгор Грушевий <yegorgru@gmail.com>
2020-11-15 17:40:40 +0200

    code decomposition

---
+++ Lab3SFML/GameRender.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "GameModel.h"
+
+class GameRender
+{
+public:
+	GameRender(GameModel& game);
+	sf::RenderWindow& window();
+	void draw();
+
+	int get_element_size();
+
+private:
+	GameModel& m_game;
+
+	sf::RenderWindow m_window;
+
+	sf::Texture texture;
+	sf::Texture background_texture;
+
+	sf::Sprite sprite;
+	sf::Sprite background_sprite;
+
+	sf::Font font;
+
+	sf::Text up_text;
+	sf::Text red_won;
+	sf::Text blue_won;
+	int w;
+};
+
